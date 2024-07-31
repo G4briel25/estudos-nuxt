fb7efd2ffe0fcfbb3dd943bb25178a24a8633dbb
Gabriel Empresa <warmerishoth@gmail.com>
2024-07-31 17:35:42 -0400

    middleware login criado

--- pages/videos/[id].vue
+++ pages/videos/[id].vue
@@ -9,12 +9,13 @@
 <script setup>
 definePageMeta({
     layout: 'exibicao',
-    middleware: 'video'
+    middleware: ['video', 'auth']
 })
 
 const route = useRoute();
 
 const idParams = computed(() => route.params.id);
+
 </script>
 
 <style lang="scss" scoped>
